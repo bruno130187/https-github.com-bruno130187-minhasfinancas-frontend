8122d2f713d23181109eef27adc39308d4827101
bruno130187 <bruno130187@gmail.com>
2023-05-29 11:57:37 -0300

    enviando jwt

--- src/components/CountDownTimer.js
+++ src/components/CountDownTimer.js
@@ -3,26 +3,19 @@ import CountDownDisplay from './CountDownDisplay';
 import UseCountDown from '../app/service/UseCountDown';
 import { ProvedorAutenticacaoContext } from "../main/ProvedorAutenticacaoContext";
 
-const ExpiredNotice = () => {
-  return (
-    <div className="expired-notice">
-      <span>Sessão expirada!</span>
-    </div>
-  );
-};
-
-const ShowCounter = ({ minutes, seconds }) => {
+const ShowCounter = ({ minutes, seconds, cor }) => {
   return (
     <div className="show-counter">
       <a
         className="countdown-link"
+        style={{color: cor}}
         href='#'
         rel="noopener noreferrer"
         target="_blank"
       >
-        <CountDownDisplay isDanger={false} type="min" value={minutes} />
+        <CountDownDisplay isDanger={false} type="min" value={minutes.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})} />
         <p>:</p>
-        <CountDownDisplay isDanger={false} type="seg" value={seconds} />
+        <CountDownDisplay isDanger={false} type="seg" value={seconds.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})} />
       </a>
     </div>
   );
@@ -33,6 +26,7 @@ const CountDownTimer = () => {
 
   var minutes = null;
   var seconds = null;
+  var cor = '';
 
   [minutes, seconds] = UseCountDown(new Date(auth.expiration));
 
@@ -40,7 +34,12 @@ const CountDownTimer = () => {
     if ((minutes + seconds) === 0) {
       encerrarSessao();
     } else {
-      return <ShowCounter minutes={minutes} seconds={seconds} />;
+      if (((minutes * 60) + seconds) < 300) {
+        cor = 'yellow';
+      } else {
+        cor = '#ececec';
+      }
+      return <ShowCounter minutes={minutes} seconds={seconds} cor={cor}/>;
     }
   }
 
